b618cbd732d47024f705f840a6ca9f8832222c09
Sreeginy Kodeeswaran <sreeginy98@gmail.com>
2024-05-14 01:04:06 +0530

    create model, database class

---
+++ app/src/main/java/com/example/thenotesapp/database/NoteDatabase.kt
@@ -0,0 +1,4 @@
+package com.example.thenotesapp.database
+
+class NoteDatabase {
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,16 +2,18 @@
 buildscript {
     repositories {
         google()
+        jcenter()
     }
 
     dependencies {
         val navVersion = "2.7.5"
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
-        classpath("com.android.tools.build:gradle:7.3.1")
+        classpath("com.android.tools.build:gradle:7.4.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
         classpath("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.9.0-1.0.13")
     }
 }
+
 
 plugins {
     id("com.android.application") version "7.3.1" apply false
