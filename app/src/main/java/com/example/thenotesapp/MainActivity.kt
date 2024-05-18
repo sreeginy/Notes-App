2d56f7c5252c2ac222086aec8f745469fccd57fe
Sreeginy Kodeeswaran <sreeginy98@gmail.com>
2024-05-18 21:32:03 +0530

    setup ViewModel

--- app/src/main/java/com/example/thenotesapp/MainActivity.kt
+++ app/src/main/java/com/example/thenotesapp/MainActivity.kt
@@ -5,17 +5,30 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
 import com.example.notesroompractice.R
+import com.example.thenotesapp.database.NoteDatabase
+import com.example.thenotesapp.repository.NoteRepository
+import com.example.thenotesapp.viewModel.NoteViewModel
+import com.example.thenotesapp.viewModel.NoteViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+
+    lateinit var noteViewModel: NoteViewModel
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        setupViewModel()
         }
+
+    private fun setupViewModel() {
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelProviderFactory = NoteViewModelFactory(application, noteRepository)
+        noteViewModel = ViewModelProvider(this, viewModelProviderFactory)[NoteViewModel::class.java]
     }
-}
+    }
--- app/src/main/java/com/example/thenotesapp/viewModel/NoteViewModelFactory.kt
+++ app/src/main/java/com/example/thenotesapp/viewModel/NoteViewModelFactory.kt
@@ -1,8 +1,13 @@
 package com.example.thenotesapp.viewModel
 
 import android.app.Application
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.thenotesapp.repository.NoteRepository
 
-class NoteViewModelFactory(val app: Application, private val noteRespository: NoteRepository) : ViewModelProvider.{
+class NoteViewModelFactory(val app: Application, private val noteRespository: NoteRepository) : ViewModelProvider.Factory{
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return NoteViewModel(app, noteRespository) as T
+    }
 }
